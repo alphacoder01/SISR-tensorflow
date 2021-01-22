d0efc62fb2ba4c14d5dc170f0eec976d241690ea
Ashish Goswami <ashishgoswami8956230@gmal.com>
2021-01-22 14:56:36 +0530

    added license

--- dataloader.py
+++ dataloader.py
@@ -1,36 +1,43 @@
 from tensorflow.python.data.experimental import AUTOTUNE
 import os
 
+'''
+ Code from @ https://github.com/krasserm/super-resolution/blob/master/data.py
+'''
+
+from tensorflow.python.data.experimental import AUTOTUNE
+
+
 class DIV2K:
     def __init__(self,
                  scale=2,
                  subset='train',
                  downgrade='bicubic',
-                 images_dir = './div2k/images',
-                 caches_dir = './div2k/caches'):
-        
-        self._nitre_2018 = True
-        
-        # _scales = [2,3,4,8]
-        # print("Available Scales = ", _scales)
-        # if scale in _scales:
-        #     self.scale = scale
-        # else:
-        #     raise ValueError(f'scale must be in ${_scales}')
-        
-        if subset == 'train':
-            self.image_ids = range(1,801)
-        elif subset == 'valid':
-            self.image_ids = range(801,901)
+                 images_dir='.div2k/images',
+                 caches_dir='.div2k/caches'):
+
+        self._ntire_2018 = True
+
+        _scales = [2, 3, 4, 8]
+
+        if scale in _scales:
+            self.scale = scale
         else:
-            raise ValueError("subset must be 'train' or 'valid'.")
-        
-        _downgrades_a = ['bicubic','unknown']
-        _downgrades_b = ['mild','difficult']
-        
-        if scale == 8 and downgrade != bicubic:
-            raise ValueError(f'scale 8 only allowed for bicubic compression.')
-        
+            raise ValueError(f'scale must be in ${_scales}')
+
+        if subset == 'train':
+            self.image_ids = range(1, 801)
+        elif subset == 'valid':
+            self.image_ids = range(801, 901)
+        else:
+            raise ValueError("subset must be 'train' or 'valid'")
+
+        _downgrades_a = ['bicubic', 'unknown']
+        _downgrades_b = ['mild', 'difficult']
+
+        if scale == 8 and downgrade != 'bicubic':
+            raise ValueError(f'scale 8 only allowed for bicubic downgrade')
+
         if downgrade in _downgrades_b and scale != 4:
             raise ValueError(f'{downgrade} downgrade requires scale 4')
 
@@ -41,48 +48,50 @@ class DIV2K:
         else:
             self.downgrade = downgrade
             self._ntire_2018 = False
-             
+
         self.subset = subset
         self.images_dir = images_dir
         self.caches_dir = caches_dir
-        
+
         os.makedirs(images_dir, exist_ok=True)
         os.makedirs(caches_dir, exist_ok=True)
-        
+
     def __len__(self):
         return len(self.image_ids)
-        
-    def dataset(self, batch_size = 16, repeat_count = None, random_transform=True):
+
+    def dataset(self, batch_size=16, repeat_count=None, random_transform=True):
         ds = tf.data.Dataset.zip((self.lr_dataset(), self.hr_dataset()))
         if random_transform:
-            ds = ds.map(lambda lr, hr: random_crop(lr, hr, scale=self.scale), num_parallel_calls = AUTOTUNE)
+            ds = ds.map(lambda lr, hr: random_crop(lr, hr, scale=self.scale), num_parallel_calls=AUTOTUNE)
             ds = ds.map(random_rotate, num_parallel_calls=AUTOTUNE)
             ds = ds.map(random_flip, num_parallel_calls=AUTOTUNE)
-                
         ds = ds.batch(batch_size)
         ds = ds.repeat(repeat_count)
-        ds = ds.prefetch(buffer_size = AUTOTUNE)
+        ds = ds.prefetch(buffer_size=AUTOTUNE)
         return ds
-        
+
     def hr_dataset(self):
         if not os.path.exists(self._hr_images_dir()):
             download_archive(self._hr_images_archive(), self.images_dir, extract=True)
-        
-        ds = self._images_dataset(self._hr_image_files()).cache(self._hr_cache_files())
+
+        ds = self._images_dataset(self._hr_image_files()).cache(self._hr_cache_file())
+
         if not os.path.exists(self._hr_cache_index()):
             self._populate_cache(ds, self._hr_cache_file())
 
         return ds
-    
+
     def lr_dataset(self):
         if not os.path.exists(self._lr_images_dir()):
             download_archive(self._lr_images_archive(), self.images_dir, extract=True)
-                
+
         ds = self._images_dataset(self._lr_image_files()).cache(self._lr_cache_file())
+
         if not os.path.exists(self._lr_cache_index()):
             self._populate_cache(ds, self._lr_cache_file())
+
         return ds
-        
+
     def _hr_cache_file(self):
         return os.path.join(self.caches_dir, f'DIV2K_{self.subset}_HR.cache')
 
